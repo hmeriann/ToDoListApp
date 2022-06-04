61edbde6f88f4a4a6cf54e2d080769fc867281ba
zuleykha.pavlichenkova <ya-zu@yandex.ru>
2022-06-04 16:49:26 +0300

    lesson 40.1

---
+++ ToDoList/ToDoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoList
+//
+//  Created by Heidi Merianne on 6/4/22.
+//  Copyright © 2022 Zuleykha Pavlichenkova aka hmeriann. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
